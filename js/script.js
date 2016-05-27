ad5f6f7244cf3709bccdedb1477e560cb17f3f47
jeffmeng <jkmexpress@yahoo.com>
2016-05-27 23:50:12 +0000

    bug-fix

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-$(function() {
-    var loading = $('#loadbar').hide();
-    $(document)
-        .ajaxStart(function() {
-            loading.show();
-        }).ajaxStop(function() {
-            loading.hide();
-        });
+/* global jquery $ */
 
-    $("label.btn").on('click', function() {
-        var choice = $(this).find('input:radio').val();
-        $('#loadbar').show();
-        $('#quiz').fadeOut();
-        setTimeout(function() {
-            $("#answer").html($(this).checking(choice));
-            $('#quiz').show();
-            $('#loadbar').fadeOut();
-            /* something else */
-        }, 1500);
+var loading = $('#loadbar').hide();
+$(document)
+    .ajaxStart(function() {
+        loading.show();
+    }).ajaxStop(function() {
+        loading.hide();
     });
 
-    $ans = 3;
+$("label.btn").on('click', function() {
+    var choice = $(this).find('input:radio').val();
+    $('#quiz').fadeOut();
+    // get answers
 
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
-        else
-            return 'CORRECT';
-    };
+    $('#quiz').show();
+
+
 });
+
+var answer = 3;
+
+function check(ck) {
+    if (ck != answer)
+        return 'INCORRECT';
+    else
+        return 'CORRECT';
+};
